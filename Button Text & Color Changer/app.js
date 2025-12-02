782208d9eca687ee9d82ee8786ed1b3cc8113ef7
Khadija <khadija@Khadijas-MacBook-Pro.local>
2025-12-03 00:35:26 +0100

    first commit

---
+++ Button Text & Color Changer/app.js
@@ -0,0 +1,11 @@
+const bntChange = document.querySelector('.btn')
+bntChange.addEventListener("click", change)
+
+function change () {
+    bntChange.style.backgroundColor = "red";  
+    bntChange.style.color = "white"; 
+    bntChange.style.backgroundColor = "red";
+
+
+}
+
